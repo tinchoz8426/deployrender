f7996e88e5bacb786ed56e29f81ccd330d94669d
tinchoz8426 <tinchoz8426@gmail.com>
2023-04-15 23:51:55 -0300

    arreglo 2

--- proyecto51325/urls.py
+++ proyecto51325/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path("", inicio),
     path("AppCoder/", include("AppCoder.urls")),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#         urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
